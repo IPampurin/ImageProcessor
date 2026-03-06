5dd700b76bafb81ee56d7cf1554ab04f92499e68
IPampurin <I.Pampurin@yandex.ru>
2026-03-06 11:24:12 +0500

    fix struct, server run

---
+++ cmd/manager/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/IPampurin/ImageProcessor/pkg/manager/configuration"
+	"github.com/IPampurin/ImageProcessor/pkg/manager/server"
+	"github.com/wb-go/wbf/logger"
+)
+
+func main() {
+
+	// cоздаём контекст
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// запускаем горутину обработки сигналов
+	go signalHandler(ctx, cancel)
+
+	// считываем .env файл
+	cfg, err := configuration.ReadConfig()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки конфигурации: %v", err)
+	}
+
+	// настраиваем логгер
+	appLogger, err := logger.InitLogger(
+		logger.ZapEngine,
+		"ImageProcessor",
+		os.Getenv("APP_ENV"), // пока оставим пустым
+		logger.WithLevel(logger.InfoLevel),
+	)
+	if err != nil {
+		log.Fatalf("Ошибка создания логгера: %v", err)
+	}
+	defer func() { _ = appLogger.(*logger.ZapAdapter) }()
+	/*
+		// получаем экземпляр БД
+		storageDB, err := db.InitDB(ctx, &cfg.DB, appLogger)
+		if err != nil {
+			appLogger.Error("ошибка подключения к БД", "error", err)
+			return
+		}
+		defer func() { _ = db.CloseDB(storageDB) }()
+
+		storageS3, err := s3.InitS3(ctx, &cfg.S3, appLogger)
+		if err != nil {
+			appLogger.Error("ошибка подключения к внешнему S3 хранилищу", "error", err)
+			return
+		}
+		defer func() { _ = s3.CloseDB(storageS3) }()
+
+		// получаем экземпляр слоя бизнес-логики
+		service := service.InitService(ctx, storageDB)
+	*/
+	// запускаем сервер
+	err = server.Run(ctx, &cfg.Server, appLogger)
+	if err != nil {
+		appLogger.Error("Ошибка сервера", "error", err)
+		cancel()
+		return
+	}
+
+	appLogger.Info("Приложение корректно завершено")
+}
+
+// signalHandler обрабатывет сигналы отмены
+func signalHandler(ctx context.Context, cancel context.CancelFunc) {
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	defer signal.Stop(sigChan)
+
+	select {
+	case <-ctx.Done():
+		return
+	case <-sigChan:
+		cancel()
+		return
+	}
+}
---
+++ pkg/manager/configuration/config.go
@@ -0,0 +1,44 @@
+package configuration
+
+import (
+	cleanenvport "github.com/wb-go/wbf/config/cleanenv-port"
+)
+
+// ConfServer — параметры HTTP-сервера
+type ConfServer struct {
+	HostName string `env:"SERVICE_HOST_NAME" env-default:"localhost"`
+	Port     int    `env:"SERVICE_PORT"       env-default:"8081"`
+	GinMode  string `env:"GIN_MODE"           env-default:"debug"`
+}
+
+// ConfDB — параметры подключения к PostgreSQL
+type ConfDB struct {
+	HostName string `env:"DB_HOST_NAME" env-default:"dbPostgres"`
+	Port     int    `env:"DB_PORT"      env-default:"5432"`
+	Name     string `env:"DB_NAME"      env-default:"db-postgres"`
+	User     string `env:"DB_USER"      env-default:"postgres"`
+	Password string `env:"DB_PASSWORD"  env-default:"postgres"`
+}
+
+// ConfS3 - параметры подключения к внешнему S3 хранилищу
+type ConfS3 struct {
+}
+
+// Config — корневая структура конфигурации
+type Config struct {
+	Server ConfServer
+	DB     ConfDB
+}
+
+// ReadConfig загружает .env файл из корня проекта и возвращает заполненную структуру Config
+func ReadConfig() (*Config, error) {
+
+	var config Config
+
+	// загружаем конфигурацию из файла .env напрямую в структуру
+	if err := cleanenvport.LoadPath("./.env", &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
